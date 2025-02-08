0b700971766ce68cc759043b472c40463dae1043
visalahmad <133162303+visalahmad@users.noreply.github.com>
2025-02-08 23:00:50 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CardHeader from '@mui/material/CardHeader'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Button from '@mui/material/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState('')
+
+  function addTodo(){
+    setTodos([...todos, todo])
+    setTodo('');
+  }
+
+  function removeTodo(item){
+    setTodos(todos.filter(t => t !== item))
+  }
+
+  function handleChange(e){
+    console.log(e.target.value);
+    setTodo(e.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <input type="text" value={todo} onChange={handleChange}/>
+      <button onClick={addTodo}>Add</button>
+      <div className="App">
+        <h1>Todo List</h1>
+        <ul className="todo-list">
+           {todos.map(i => <li onClick={() => removeTodo(i)}>{i}</li>)}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
